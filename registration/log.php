9ce4084de8bd323659c1f7c51bfc9ff6b7cc79c5
Tisha-23 <106397418+Tisha-23@users.noreply.github.com>
2022-06-15 23:18:06 +0600

    Add files via upload

---
+++ registration/log.php
@@ -0,0 +1,69 @@
+<div >
+    <center><img src="salon3.jpg" height=230 width=1300 style="opacity:0.8" ></center>
+    <br>
+   <center> <h1 style="color:rgb(191,0,255)">b'you beauty salon</center>
+   <center><p style="color:rgb(171,60,12)">Make time for yourself</center>
+   
+</div>
+<style>
+table, th, td {
+
+}
+
+table.center {
+  margin-left: auto; 
+  margin-right: auto;
+}
+</style>
+<table class="center">
+    <tr>
+        <td>
+        
+        <th></th>
+        <th></th>
+         <td>
+        </tr>
+    <tr>
+    <td>Username:</td>
+    <td><input type="text" id="uname" name="uname"></td><br><br>
+</tr>
+    <tr>
+   <td> Password:</td>
+   <td><input type="password" id="pass" name="pass"></td>
+</tr>
+
+</table>
+
+   <center> Gender:
+   <input type="radio" id="gender" name="gender" value="Male">Male
+
+
+   <input type="radio" id="gender" name="gender" value="Female">Female
+
+   <input type="radio" id="gender" name="gender" value="Others">Others<br></br>
+</center>
+
+<center>
+    Skills:
+    <input type="checkbox" id="skill[]" value="Base makeup">Base makeup
+    <input type="checkbox" id="skill[]" value="Hair cutting">Hair cutting
+    <input type="checkbox" id="skill[]" value="Flawless makeup">Flawless makeup
+   <input type="checkbox" id="skill[]" value="Rebonding">Rebonding
+</center>
+   <br>
+   <br>
+   <center> 
+   Service type :<select name="Service">
+    <option value="Bridal">Bridal</option>
+    <option value="Party makeup">Party makeup</option>
+    <option value="Hair style">Hair style</option>
+    <option value="Nail extention">Nail extention</option>
+    <option value="Facial">Facial</option>
+</select>
+</center>
+<table class="center">
+<tr>
+<td>Present Address:</td>
+    <td><textarea rowspan="4" colspan="40"></textarea></td><br><br>
+</tr>
+</table>
